9fc6736d55b355f71ee439ba71dc166f5b9324e7
tanzir08 <tanzir.b@gmail.com>
2012-06-26 02:25:39 +0600

    site controller uodates by tanzir

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -1,22 +1,20 @@
 <?php
-class Site extends CI_Controller{
-<<<<<<< HEAD
+
+class Site extends CI_Controller {
+
     function index() {
         echo "Hello dude!";
         echo "this is siddhartha";
         echo "this is Tanzir";
         echo "inside a deleted function";
-
     }
-}
 
-=======
-    
->>>>>>> d4d45d84065eb7dacb0a80d5c1d5e219e31f9c07
-function dosomething() {
-    echo "this is arafat";
-    echo "this is second arafat";
-}
+    function dosomething() {
+        echo "this is arafat";
+        echo "this is second arafat";
+        echo "this is tanzir";
+    }
 
 }
+
 ?>
