5e532186e465265ce4eba161d1c4629e2b145858
unknown <BSADD@BUET.(none)>
2012-06-28 12:37:27 +0600

    login

--- application/views/user_page.php
+++ application/views/user_page.php
@@ -1,1 +1,17 @@
-user home!
+
+<?php 
+    $userid_session = $this->session->userdata('name');
+    
+    echo "Hello  ".$userid_session;
+?>
+<br/>
+<P><?php echo anchor('authenticate/logout','Log Out');?></P>
+<hr />
+
+<p><?php  echo anchor('search/searchArtist','searchArtist'); ?></p>
+<p><?php echo anchor('search/searchAlbum','searchAlbum'); ?></p>
+<P><?php echo anchor('search/searchTrack','searchTrack');?></P>
+
+
+
+
