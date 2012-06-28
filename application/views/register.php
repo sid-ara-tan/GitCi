e0ba98235eb6806f51b9c6ddc9a37f8b7fb3651e
unknown <BSADD@BUET.(none)>
2012-06-28 11:42:54 +0600

    user can register now

--- application/views/register.php
+++ application/views/register.php
@@ -7,30 +7,35 @@
     </head>
     
     <body>
-        <?php echo heading($info, 3, 'class="pink"'); ?>
-
+        
         <div>
             <form action="<?php echo base_url(); ?>index.php/authenticate/validate_register" method="post" accept-charset="utf-8" id="myform1">
 
 
                 <div id="signup_form">
-                    Full name:
-                    <input type="text" name="name" maxlength="30" value="" />
+                   <?php   echo form_open('s_login/s_create_student_account'); ?>
 
-      
-                    User Name:
-                    <input type="text" name="username" maxlength="50" value="" />
-
-
-                    Password:
-                    <input type="password" name="password" maxlength="30" value="" />
-                 
-  
-                    Confirm Password:
-                    <input type="password" name="password2" maxlength="30" value="" />
+                    <P><label for="name">Full Name:</label><?php  echo form_input('name',set_value('name')); ?></P>
+                    <p><?php echo form_error('name','<p class="error">'); ?></p>
                     
-                    <input type="submit" value="Register" /></td>
+                    <P><label for="name">User Name:</label><?php  echo form_input('username',set_value('username')); ?></P>
+                    <p><?php echo form_error('username','<p class="error">'); ?></p>
+                    
+
+                    <p><label for="name">Password:</label><?php   echo form_password('password',set_value('password')); ?></p>
+                    <p><?php echo form_error('password','<p class="error">'); ?></p>
+
+                    <p><label for="name">Confirm Password:</label><?php   echo form_password('password2',set_value('password2')); ?></p>
+                    <p><?php echo form_error('password2','<p class="error">'); ?></p>
                 
+                    <p><label for="name">Credit Card NO(exactly 16 digit):</label><?php   echo form_input('credit_no',set_value('credit_no')); ?></p>
+                    <p><?php echo form_error('credit_no','<p class="error">'); ?></p>
+                
+                    
+                    <p><?php   echo form_submit('submit','Create Account');?></p>
+                    
+                    <p class="error"><?php echo $valid_message;?></p>
+                    
                 </div>
 
             </form>
